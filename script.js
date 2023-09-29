e2063a1e8aae86e0269c0bd5760f60676d68ce07
Luis Fernando <100451426+Luisokl@users.noreply.github.com>
2023-09-29 00:27:13 -0300

    selecionando elementos no html para manipulação

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const inputElement = document.querySelector(".addtarefa #text");
+let taskElement = document.querySelector('#app input');
+let buttonAdd = document.querySelector('#app button');
+let listElement = document.querySelector ('#body ul');
+
+let tarefas = [];
+
+
+function adicionarTarefas() {
+    if (taskElement.value === '') {
+        alert('adicione uma tarefa..');
+    } else {
+        let tasksElement = taskElement.value;
+        tarefas.push(tasksElement);
+
+        console.log(tarefas);
+    }
+}
+
+
+buttonAdd.addEventListener('click', adicionarTarefas);
