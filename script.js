3e70448ee3347f21df7b580c8c475c765b75e048
Luis Fernando <100451426+Luisokl@users.noreply.github.com>
2023-09-30 21:33:18 -0300

    adicionando função que deleta itens da lista

--- script.js
+++ script.js
@@ -14,17 +14,30 @@ function adicionarTarefas() {
     } else {
         let tasksElement = taskElement.value;
         taskElement.value = ''
-        
-        tarefas.push(tasksElement);
-
-        tarefas.map(task => {
-            let liElement = document.createElement('li');
-            liElement.textContent = task;
-            let buttonRemover = document.createElement('button');
-            buttonRemover.textContent = 'Excluir';
-            liElement.appendChild(buttonRemover);
-            liElement.style.listStyleType = 'none'
-            listElement.appendChild(liElement);
-        }) 
+        tarefas.push(tasksElement);        
+        renderizarTela();
     }
 }
+
+function renderizarTela() {
+    listElement.innerHTML ='';
+    
+    tarefas.map((task, index) => {
+        let liElement = document.createElement('li');
+        liElement.textContent = task;
+        let buttonRemover = document.createElement('button');
+        buttonRemover.textContent = 'Excluir';
+        liElement.appendChild(buttonRemover);
+        liElement.style.listStyleType = 'none'
+        listElement.appendChild(liElement);
+
+        buttonRemover.addEventListener('click', (index)=> {
+            deletarTarefa(index);
+        });
+    }) 
+}
+
+function deletarTarefa(index) {
+    tarefas.splice(index, 1);
+    renderizarTela();
+};
