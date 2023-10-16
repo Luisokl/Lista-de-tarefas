6b11047257b4448504a41a2a8aa270dbc2c3d8d1
Luis Fernando <luis.melo.okl@outlook.com>
2023-10-15 22:28:46 -0300

    modificação na tentativa de encontrar a solução para o erro na função de editar a tarefa

--- script.js
+++ script.js
@@ -28,18 +28,58 @@ function renderizarTela() {
         let liElement = document.createElement('li');
         liElement.appendChild(divElement);
 
-
+        let divbuttons = document.createElement('div');
         let buttonRemover = document.createElement('button');
-        buttonRemover.textContent = 'Excluir';
-        liElement.appendChild(buttonRemover);
+        let buttonEditar = document.createElement('button');
+        let imageLixeira = document.createElement('img');
+        let imagePencil = document.createElement('img');
+        imagePencil.src = '/img/lapis.png'
+        imageLixeira.src = '/img/excluir.png';
+        buttonRemover.appendChild(imageLixeira); 
+        buttonEditar.appendChild(imagePencil);
+        divbuttons.appendChild(buttonEditar);
+        divbuttons.appendChild(buttonRemover);
+        liElement.appendChild(divbuttons);
         liElement.style.listStyleType = 'none'
 
         listElement.appendChild(liElement);
 
-        buttonRemover.addEventListener('click', (index)=> {
+        buttonEditar.addEventListener('click', () => {
+            const index = tarefas.indexOf(task);
+            editarTarefa(index, listElement);
+            
+        })
+
+        buttonRemover.addEventListener('click', () => {
+            const index = tarefas.indexOf(task);
             deletarTarefa(index);
         });
     }) 
+}
+
+function editarTarefa(index, listElement) {
+    let liElement = listElement.children[index];
+    let divElement = liElement.children[0];
+    let divbuttons = liElement.children[1];
+
+    let inputEdit = document.createElement('input');
+    inputEdit.value = divElement.textContent;
+    divElement.textContent = '';
+    divElement.appendChild(inputEdit);
+
+    let buttonConfirm = document.createElement('button');
+    buttonConfirm.textContent = 'confirmar'
+    divbuttons.appendChild(buttonConfirm);
+
+    buttonConfirm.removeEventListener('click', confirmEdit);
+    buttonConfirm.addEventListener('click', confirmEdit);
+
+    function confirmEdit() {
+        tarefas[index] = inputEdit.value;
+        divElement.textContent = inputEdit.value;
+        inputEdit.remove();
+        buttonConfirm.remove();
+    }
 }
 
 function deletarTarefa(index) {
